9bb05c5a394d389df50da08d79ce270383cc1b37
brokiem <milyhqvideo@gmail.com>
2023-01-14 21:21:16 +0800

    use puppeteer

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,12 +1,29 @@
 import axios, { AxiosRequestConfig } from "axios";
+import edgeChromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 
 const axiosInstance = axios.create()
 const fetcher = async (url: string) => {
-    const axiosConfig:AxiosRequestConfig = {
-        method: "GET",
-        url: url
+    const executablePath = await edgeChromium.executablePath;
+
+    const browser = await puppeteer.launch({
+        executablePath,
+        args: edgeChromium.args,
+        headless: false,
+    })
+
+    const page = await browser.newPage()
+    await page.goto(url)
+
+    const content = await page.content()
+
+    await browser.close();
+
+    const response = {
+        status: 200,
+        data: content
     }
-    const getters = await axiosInstance.request(axiosConfig)
-    return Promise.resolve(getters)
+
+    return Promise.resolve(response)
 }
 export default fetcher
